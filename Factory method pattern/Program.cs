6d4534bc35f37874cde23e46b8f1199e00e831d3
Joey <joeyvandervalk@gmail.com>
2023-03-25 16:54:43 +0100

    Added factory method pattern.

---
+++ Factory method pattern/Program.cs
@@ -0,0 +1,42 @@
+﻿// Factory method pattern
+using System;
+
+
+namespace FactoryMethodPattern
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new HotelReservationFactory();
+            var reservation = factory.CreateReservation();
+            reservation.Book();
+            Console.WriteLine(reservation.IsBooked);
+        }
+    }
+
+
+    public interface Reservation
+    {
+        bool IsBooked { get; }
+        void Book();
+    }
+
+    public class HotelReservation : Reservation
+    {
+        public bool IsBooked { get; private set; }
+
+        public HotelReservation(bool IsBooked) => this.IsBooked = IsBooked;
+        public void Book() { IsBooked = true; }
+    }
+
+    public interface ReservationFactory
+    {
+        Reservation CreateReservation();
+    }
+
+    public class HotelReservationFactory : ReservationFactory
+    {
+        public Reservation CreateReservation() => new HotelReservation(false);
+    }
+}
